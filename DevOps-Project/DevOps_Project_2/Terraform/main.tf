845bd16c27726ed7771509594f8d6449b30c5d81
joanroamora <41650757+joanroamora@users.noreply.github.com>
2023-08-02 22:50:58 -0500

    Updating security group to fix an issue related with ssh permissions

--- DevOps-Project/DevOps_Project_2/Terraform/main.tf
+++ DevOps-Project/DevOps_Project_2/Terraform/main.tf
@@ -12,14 +12,33 @@ data "aws_ami" "amazon-linux" {
   }
 }
 
+resource "aws_security_group" "basic_security" {
+    ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "ssh"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+
+    egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+ }
+}
+
 resource "aws_instance" "dev_machine" {
   ami = data.aws_ami.amazon-linux.id
   instance_type = "t2.micro"
   #key_name = "jroamkeys"
   key_name = "${var.keypair}"
+  vpc_security_group_ids = [aws_security_group.basic_security.id]
 
   tags = {
     Environment = "dev"
     Name = "${var.name}-server"
   }
 }
+
+
